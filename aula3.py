0d7b919ec940835dd5761c7c838337b685b7b153
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-05 11:41:47 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,6 @@
+#tipos de dados primitivos
+#int para numeros inteiros. ex;7.0.-4
+#float para numeros reais ou com virgulas ex;0.45 .7.6
+#bool para valores logicos de verdadeiro ou falso.ex;true ou false sempre uarcom letras maiusculas
+#str para palavras que são palavras caracteries ou textos. ex; "olá"."7.5"
+#para saber o tipo de uma variavel pode se usar o comando print(type(variavel))
